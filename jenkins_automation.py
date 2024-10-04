117d0760d56097215d21b8bf125d164510a3321d
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-04 11:33:00 +0200

    createVM funktioniert

--- jenkins_automation.py
+++ jenkins_automation.py
@@ -34,10 +34,9 @@ class VMManager:
 
 def main():
     api_token = "2qqLRyCJcWatOJuW46CQ0mvyaPxBkboh7fJxjSVrcsxEGVwAJDeR5RgO7vZ2PfwZ"
-    os_type = input("Enter the OS type: ")
-    server_type = input("Enter the server type: ")
-    ssh_key = input("Enter your SSH key: ")
-
+    os_type = "ubuntu-24.04"
+    server_type = "cx22"
+    ssh_key = "AzureAD+ClemensNikolaus@CQ-Clemens"
     manager = VMManager(api_token)
     manager.create_vm(os_type, server_type, ssh_key)
 
