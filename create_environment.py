3d7ebae5710cb5837826878a697360f774d0145e
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-08 13:41:48 +0200

    add jenkinsfile_setup for create_environment.py

--- create_environment.py
+++ create_environment.py
@@ -2,23 +2,26 @@ from jenkins_automation import VMManager, EnvironmentManager, DNSManager
 import os
 
 def main():
-    api_token = "2qqLRyCJcWatOJuW46CQ0mvyaPxBkboh7fJxjSVrcsxEGVwAJDeR5RgO7vZ2PfwZ"
-    dns_api_token = "LGoghBCjh7HP0CB2ONqmHdD0POag2yLA"
-    domain = "comquent.academy"
+    api_token = os.getenv('API_TOKEN')
+    dns_api_token = os.getenv('DNS_API_TOKEN')
+    domain = os.getenv('DOMAIN')
+    ssh_private_key_path = os.getenv('SSH_PRIVATE_KEY_PATH')
+
     os_type = "ubuntu-22.04"
     server_type = "cx22"
     ssh_key_id = 23404904
-    ssh_private_key_path = os.path.expanduser("~/.ssh/id_rsa")
-    ip_address = "128.140.43.141"
+
+    manager = VMManager(api_token)
+    manager.create_vm(os_type, server_type, ssh_key_id)
     
-    # manager = VMManager(api_token)
-    # manager.create_vm(os_type, server_type, ssh_key_id)
+    env_manager = EnvironmentManager(manager, ssh_private_key_path)
+    env_manager.setup_jenkins()
     
-    dnsman = DNSManager(dns_api_token)
-    url = dnsman.create_dns_record(domain, ip_address)
+    dns_manager = DNSManager(dns_api_token)
+    ip_adress = manager.get_vm_ip()
+    dns_manager.create_dns_record(domain, ip_adress)
     
-    # env_manager = EnvironmentManager(manager, ssh_private_key_path)
-    # env_manager.setup_jenkins()
+    env_manager.setup_nginx(domain)
     
     
 if __name__ == "__main__":
