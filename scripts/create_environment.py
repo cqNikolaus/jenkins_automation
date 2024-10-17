5a141b50f10157f55632e5339e02deb111f6c8b2
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-17 10:22:31 +0200

    fix

--- scripts/create_environment.py
+++ scripts/create_environment.py
@@ -60,5 +60,8 @@ def main():
     except Exception as e:
         print(f"Error setting up the environment: {e}")
         sys.exit(1)
+        
+        
+if __name__ == "__main__":        
     main()
     
