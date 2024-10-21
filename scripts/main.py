68c5aab0593fd4ded336c85d658e6511ce106a9c
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-21 12:05:16 +0200

    debug admin credentials

--- scripts/main.py
+++ scripts/main.py
@@ -44,6 +44,7 @@ def main():
     manager = VMManager(api_token)
     env_manager = EnvironmentManager(manager, key_file, jenkins_user, jenkins_pass, job_name)
     
+    print(f"jenkins user: {jenkins_user}")
     
     if args.command == 'create_jenkins':
         manager.create_vm(os_type, server_type, ssh_key)
