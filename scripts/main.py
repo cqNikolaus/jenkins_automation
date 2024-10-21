929ee58d66d74bd120947c2b74d5c44390d9a4ec
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-21 14:24:36 +0200

    debug ssh key handling

--- scripts/main.py
+++ scripts/main.py
@@ -32,10 +32,12 @@ def main():
     jenkins_user = os.getenv('JENKINS_USER')
     jenkins_pass = os.getenv('JENKINS_PASS')
     domain = os.getenv('DOMAIN')
-    ssh_private_key = os.getenv('SSH_PRIVATE_KEY')
+    ssh_private_key = os.getenv('SSH_KEY_FILE')
     zone_name = os.getenv('ZONE_NAME')
     ssh_key = os.getenv('SSH_KEY_NAME')
     job_name = os.getenv('JOB_NAME')
+    
+    print(f"ssh_private_key: {ssh_private_key}")
     
     os_type = "ubuntu-22.04"
     server_type = "cx22"
