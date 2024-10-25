8a31c5ccb488c00a37da533c4eb757866312811e
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-10-25 09:32:04 +0200

    fix

--- automation_lib/jenkins_installer.py
+++ automation_lib/jenkins_installer.py
@@ -60,13 +60,19 @@ class JenkinsInstaller:
     def clone_config_repo(self):
         self.ssh_manager.execute_command(
         f"git clone {self.config_repo_url} /var/jenkins_home/jenkins_configs")
+        
+    def read_key_file(key_file):
+        with open(key_file, 'r') as file:
+            key_content = file.read()
+        return key_content
+
 
 
     def run_jenkins_container(self):
         domain = os.getenv('DOMAIN')
         api_token_escaped = shlex.quote(self.api_token)
         dns_api_token_escaped = shlex.quote(self.dns_api_token)
-        ssh_key_escaped = shlex.quote(self.ssh_private_key)
+        ssh_key_escaped = shlex.quote(self.ssh_key_content)
         self.ssh_manager.execute_command(
             f"sudo docker run -d --name jenkins "
             f"-p 8080:8080 -p 50000:50000 "
@@ -86,4 +92,5 @@ class JenkinsInstaller:
         self.clone_config_repo()
         self.copy_dockerfile_to_vm()
         self.build_jenkins_docker_image()
+        self.ssh_key_content = self.read_key_file(self.ssh_private_key)
         self.run_jenkins_container()
