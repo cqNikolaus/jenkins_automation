d4ae577ec87da6a544e65459c579250563c3fef2
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-11-04 13:52:44 +0100

    fix reverse proxy

--- automation_lib/nginx_installer.py
+++ automation_lib/nginx_installer.py
@@ -34,7 +34,7 @@ class NginxInstaller:
             ssl_certificate_key /etc/letsencrypt/live/{self.domain}/privkey.pem;
 
             location / {{
-                proxy_pass http://localhost:8080/;
+                proxy_pass http://localhost:8080;
                 proxy_set_header Host $host;
                 proxy_set_header X-Real-IP $remote_addr;
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
