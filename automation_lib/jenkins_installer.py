aab50ee787a1423ad77f78cf756164b094691eaf
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2024-12-18 10:09:05 +0100

    Fix ubuntu docker repo

--- automation_lib/jenkins_installer.py
+++ automation_lib/jenkins_installer.py
@@ -23,17 +23,17 @@ class JenkinsInstaller:
 
     def install_docker(self):
         commands = [
-            "sudo apt-get update -y",
-            "sudo apt-get install -y ca-certificates curl gnupg lsb-release",
-            "sudo mkdir -p /etc/apt/keyrings",
-            "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
-            'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] '
-            'https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | '
-            'sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
-            "sudo apt-get update -y",
-            "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-            "sudo usermod -aG docker $USER",
-            "sudo chmod 666 /var/run/docker.sock"
+        "sudo apt-get update -y",
+        "sudo apt-get install -y ca-certificates curl gnupg lsb-release",
+        "sudo mkdir -p /etc/apt/keyrings",
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] '
+        'https://download.docker.com/linux/ubuntu jammy stable" | '
+        'sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+        "sudo apt-get update -y",
+        "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+        "sudo usermod -aG docker $USER",
+        "sudo chmod 666 /var/run/docker.sock"
         ]
         for cmd in commands:
             self.ssh_manager.execute_command(cmd)
