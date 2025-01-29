bed60fd8ce958df4091bb35ceaf5c75f9ed15c0c
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2025-01-29 13:01:00 +0100

    Fix comma

--- automation_lib/environment_manager.py
+++ automation_lib/environment_manager.py
@@ -111,7 +111,7 @@ class EnvironmentManager:
                     
                     # 4) Agent-Verzeichnis anlegen
                     f"sudo mkdir -p /home/{username}/agent",
-                    f"sudo chown -R {username}:{username} /home/{username}"
+                    f"sudo chown -R {username}:{username} /home/{username}",
                     
                     # 5) JAVA_HOME & PATH in /etc/environment setzen 
                     "echo 'JAVA_HOME=\"/usr/lib/jvm/java-17-openjdk-amd64\"' | sudo tee -a /etc/environment",
